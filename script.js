111719e4c672dc5fda2b848d771df2cf7afe48cd
haidar <mhaidarhammam@gmail.com>
2023-03-16 17:32:32 +0700

    Finish dropdown

--- script.js
+++ script.js
@@ -1,80 +1,58 @@
-var x, i, j, l, ll, selElmnt, a, b, c;
-/* Look for any elements with the class "custom-select": */
-x = document.getElementsByClassName("custom-select");
-l = x.length;
-for (i = 0; i < l; i++) {
-    selElmnt = x[i].getElementsByTagName("select")[0];
-    ll = selElmnt.length;
-  /* For each element, create a new DIV that will act as the selected item: */
-    a = document.createElement("DIV");
-    a.setAttribute("class", "select-selected");
-    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-    x[i].appendChild(a);
-  /* For each element, create a new DIV that will contain the option list: */
-    b = document.createElement("DIV");
-    b.setAttribute("class", "select-items select-hide");
-    for (j = 1; j < ll; j++) {
-    /* For each option in the original select element,
-    create a new DIV that will act as an option item: */
-    c = document.createElement("DIV");
-    c.innerHTML = selElmnt.options[j].innerHTML;
-    c.addEventListener("click", function(e) {
-        /* When an item is clicked, update the original select box,
-        and the selected item: */
-        var y, i, k, s, h, sl, yl;
-        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-        sl = s.length;
-        h = this.parentNode.previousSibling;
-        for (i = 0; i < sl; i++) {
-            if (s.options[i].innerHTML == this.innerHTML) {
-            s.selectedIndex = i;
-            h.innerHTML = this.innerHTML;
-            y = this.parentNode.getElementsByClassName("same-as-selected");
-            yl = y.length;
-            for (k = 0; k < yl; k++) {
-            y[k].removeAttribute("class");
-            }
-            this.setAttribute("class", "same-as-selected");
-            break;
-            }
-        }
-        h.click();
-    });
-    b.appendChild(c);
-    }
-    x[i].appendChild(b);
-    a.addEventListener("click", function(e) {
-    /* When the select box is clicked, close any other select boxes,
-    and open/close the current select box: */
-    e.stopPropagation();
-    closeAllSelect(this);
-    this.nextSibling.classList.toggle("select-hide");
-    this.classList.toggle("select-arrow-active");
-    });
-}
+const dropdowns = document.querySelectorAll('.dropdown');
+const semester_option = document.getElementsByClassName("option")[0]
+var semester_selected, matkul_selected;
 
-function closeAllSelect(elmnt) {
-    /* A function that will close all select boxes in the document,
-    except the current select box: */
-    var x, y, i, xl, yl, arrNo = [];
-    x = document.getElementsByClassName("select-items");
-    y = document.getElementsByClassName("select-selected");
-    xl = x.length;
-    yl = y.length;
-    for (i = 0; i < yl; i++) {
-        if (elmnt == y[i]) {
-            arrNo.push(i)
+semester_option.querySelectorAll("p").forEach(function(e, i){
+    e.addEventListener("click", function() {
+        semester_option.parentNode.firstChild.textContent = e.textContent;
+        this.style.display = "none";
+        if(semester_selected != null) {
+            semester_selected.style.display = "block";
+        }
+        document.getElementsByClassName("matkul")[0].firstChild.textContent = "Mata Kuliah:"
+        semester_selected = this;
+        matkul_selected = null
+        
+        generateMatkul(i + 1);
+    });
+});
+document.addEventListener("click", function(event, i) {
+    dropdowns.forEach(function(e) {
+        option = e.getElementsByClassName("option")[0]
+        if (!e.contains(event.target)) {
+            if (option.style.display == "block") {
+                option.style.display = "none";
+            }
         } else {
-            y[i].classList.remove("select-arrow-active");
+            if (option.style.display == "block") {
+                option.style.display = "none";
+            } else {
+                option.style.display = "block";
+            }   
         }
-    }
-    for (i = 0; i < xl; i++) {
-        if (arrNo.indexOf(i)) {
-            x[i].classList.add("select-hide");
-        }
-    }
-}
+    })
+})
 
-/* If the user clicks anywhere outside the select box,
-then close all select boxes: */
-document.addEventListener("click", closeAllSelect);
+function generateMatkul(semester) {
+    var matkul, mata_kuliah, matkul_element;
+    if (semester == 1) mata_kuliah = ["PAI", "Kalkulus", "DDAI", "Alpro Dasar", "Bahasa Inggris", "Pancasila", "DMS"];
+    if (semester == 2) mata_kuliah = ["Aljabar Linear", "Statistika", "Struktur Data", "Alpro Lanjut", "Bahasa Indonesia", "Jaringan Komputer"];
+    matkul = document.getElementsByClassName("matkul")[0].getElementsByClassName("option")[0]
+    matkul.querySelectorAll("p").forEach(function(e) {
+        e.remove();
+    });
+    mata_kuliah.forEach(function(e) {
+        matkul_element = document.createElement("p");
+        matkul_element.innerHTML = e;
+        matkul.appendChild(matkul_element);
+        matkul_element.addEventListener("click", function() {
+            matkul.parentNode.firstChild.textContent = e;
+            this.style.display = "none";
+            if(matkul_selected != null) {
+                matkul_selected.style.display = "block";
+            }
+            matkul_selected = this;
+        });
+    });
+    
+}
